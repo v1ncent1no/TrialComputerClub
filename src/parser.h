5e88cbcdac5cd58b4077672860a9f6b64c924c1a
Nikita Syrman <91823336+v1ncent1no@users.noreply.github.com>
2024-02-03 07:54:03 +0300

    quick BNF fix

--- src/parser.h
+++ src/parser.h
@@ -27,7 +27,7 @@
 //<digit> ::= '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 //<string> ::= <char> | <char> <string>
 //<char> ::= <letter> | <digit>
-//<letter> ::= 'A' | 'B' | 'C' | ... | 'Z' | 'a' | 'b' | 'c' | ... | 'z'
+//<letter> ::= 'a' | 'b' | 'c' | ... | 'z'
 //<space> ::= ' '
 //<colon> ::= ':'
 namespace parser {
